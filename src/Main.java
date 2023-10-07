1ed1e5e8af0d775ae69d716b49c032f03bfdf81a
Amirhossein Andouhgin <amirhossein.andoohgin@gmail.com>
2023-10-07 20:50:01 +0330

    menu bar color

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+        System.setProperty("apple.laf.useScreenMenuBar", "true");
+        System.setProperty("apple.awt.application.appearance", "system");
+
         GameFrame frame = new GameFrame();
     }
 }
